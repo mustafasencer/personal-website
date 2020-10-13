8f11999cbd7c36ff5f71fac72112c8ba61d10232
mustafasencerozcan <Rumansare12345>
2020-10-13 09:47:36 +0300

    component and models

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+function Footer(props: object) {
+    const [networks, setNetworks] = useState()
+    return (
+        <footer>
+            <div className="row">
+                <div className="twelve columns">
+                    <ul className="social-links">
+                        {networks}
+                    </ul>
+
+                    <ul className="copyright">
+                        <li>&copy; Copyright 2020 Mustafa Sencer Özcan</li>
+                        <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+                    </ul>
+
+                </div>
+                <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home">
+                    <i className="icon-up-open"></i></a></div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/Models/Resume.ts
@@ -0,0 +1,5 @@
+import {IMain} from "./Main";
+
+export interface IResponse {
+
+}
